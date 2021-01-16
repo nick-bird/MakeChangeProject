9e7c9f0bc84e778ec152d89ae7926977c306bbb9
Nick Bird <bird.nick.95@gmail.com>
2021-01-16 12:21:14 -0700

    Working solution

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -3,8 +3,76 @@ package makechange;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+double tendered = 143.89;
+double price = 47.51;
 
+tendered = (int) ((tendered * 100));
+price = (int) ((price * 100));
+
+System.out.println(price);
+System.out.println(tendered);
+//if (change - 20 > 0) {
+//
+//	System.out.println(change);
+//	int twenty = (int) (change / 20);
+//	System.out.println(change);
+//	change = change - (twenty * 20);
+//	System.out.println(twenty);
+//	System.out.println(change);
+//}
 	}
-
 }
+	
+//	if (change - 10 > 0) {
+//		int ten = (int) (change / 10);
+//		change = change - (ten * 10);
+//		System.out.println(ten);
+//		System.out.println(change);
+//	}
+//	if (change - 5 > 0) {
+//		int five = (int) (change / 5);
+//		change = change - (five * 5);
+//		System.out.println(five);
+//		System.out.println(change);
+//	}
+//	if (change - 1 > 0) {
+//		int one = (int) (change / 1);
+//		change = change - one;
+//		System.out.println(one);
+//		System.out.println(change);
+//	}
+//	
+//	while (change - .25 > 0) {
+////		int quarter = (int) (change / .25);
+////		while (quarter > 0) {
+//		change = change - .25;
+//		}
+//		//System.out.println(quarter);
+//		System.out.println(change);
+//	}
+//	if (change - .10 > 0) {
+//		int dimes = (int) (change / .10);
+//		change = change - (dimes * .1);
+//		System.out.println(dimes);
+//		System.out.printf("%.2f" , change);
+//	}
+//	if (change - .05 > 0) {
+//		int nickels = (int) (change / .05);
+//		change = change - (nickels * .05);
+//		System.out.println(nickels);
+//		System.out.printf("%.2f" , change);
+//	}
+//	if (change - .01 > 0) {
+//		int pennies = (int) (change / .01);
+//		change = change - (pennies * .01);
+//		System.out.println(pennies);
+//		System.out.printf("%.2f" , change);
+//	
+	
+
+
+
+
+	
+
+
