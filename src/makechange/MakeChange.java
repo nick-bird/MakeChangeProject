832a0e36f5246a4cde866760920523fe85ddb7b0
Nick Bird <bird.nick.95@gmail.com>
2021-01-16 12:22:50 -0700

    working solution

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,73 +1,105 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-double tendered = 143.89;
-double price = 47.51;
-
-tendered = (int) ((tendered * 100));
-price = (int) ((price * 100));
-
-System.out.println(price);
-System.out.println(tendered);
-//if (change - 20 > 0) {
-//
-//	System.out.println(change);
-//	int twenty = (int) (change / 20);
-//	System.out.println(change);
-//	change = change - (twenty * 20);
-//	System.out.println(twenty);
-//	System.out.println(change);
-//}
-	}
-}
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Price of item: ");
+		double price = input.nextDouble();
+		int priceCents = (int) (price * 100);
+		// convert the price from a decimal number to a whole number,
+//with each value representing one cent 
+		System.out.println("Amount tendered: ");
+		double paid = input.nextDouble();
+		int paidCents = (int) (paid * 100);
+		// convert the amount paid from a decimal number to a whole number,
+		//with each value representing one cent 
+		
+		int changeCents = paidCents - priceCents;
+		System.out.println(changeCents);
+		
+		double change = (changeCents);
+		change = change /100;
+		System.out.println(change);
+		//converts back to decimal, and divides by 100 to 
+		// represent standard monetary notation 
+		
 	
-//	if (change - 10 > 0) {
-//		int ten = (int) (change / 10);
-//		change = change - (ten * 10);
-//		System.out.println(ten);
-//		System.out.println(change);
-//	}
-//	if (change - 5 > 0) {
-//		int five = (int) (change / 5);
-//		change = change - (five * 5);
-//		System.out.println(five);
-//		System.out.println(change);
-//	}
-//	if (change - 1 > 0) {
-//		int one = (int) (change / 1);
-//		change = change - one;
-//		System.out.println(one);
-//		System.out.println(change);
-//	}
-//	
-//	while (change - .25 > 0) {
-////		int quarter = (int) (change / .25);
-////		while (quarter > 0) {
-//		change = change - .25;
-//		}
-//		//System.out.println(quarter);
-//		System.out.println(change);
-//	}
-//	if (change - .10 > 0) {
-//		int dimes = (int) (change / .10);
-//		change = change - (dimes * .1);
-//		System.out.println(dimes);
-//		System.out.printf("%.2f" , change);
-//	}
-//	if (change - .05 > 0) {
-//		int nickels = (int) (change / .05);
-//		change = change - (nickels * .05);
-//		System.out.println(nickels);
-//		System.out.printf("%.2f" , change);
-//	}
-//	if (change - .01 > 0) {
-//		int pennies = (int) (change / .01);
-//		change = change - (pennies * .01);
-//		System.out.println(pennies);
-//		System.out.printf("%.2f" , change);
-//	
+	
+		int twentys = 0;
+		int tens = 0;
+		int fives = 0;
+		int ones = 0;
+		int quarters = 0;
+		int dimes = 0;
+		int nickels = 0;
+		int pennies = 1;
+		
+		if (changeCents - 2000 > 0) {
+			twentys = changeCents / 2000; 
+			changeCents = changeCents - (twentys * 2000);
+			System.out.println(changeCents);
+		System.out.println("$20 x " + twentys);
+		
+			if (changeCents - 1000 > 0) {
+			tens = changeCents / 1000;
+			changeCents = changeCents - (tens * 1000);
+			System.out.println(changeCents);
+		System.out.println("$10 x " + tens);
+			}
+		
+			if (changeCents - 500 > 0) {
+				fives = changeCents / 500;
+				changeCents = changeCents - (fives * 500);
+				System.out.println(changeCents);
+			System.out.println("$5 x " + fives);
+		
+	}
+		if (changeCents - 100 > 0) {
+			ones = changeCents / 100;
+			changeCents = changeCents - (ones * 100);
+			System.out.println(changeCents);
+			System.out.println("$1 x " + ones);
+			
+		}
+		if (changeCents - 25 > 0) {
+			quarters = changeCents / 25;
+			changeCents = changeCents - (ones * 25);
+			System.out.println("$0.25 x " + quarters);
+			
+			
+		}
+		if (changeCents - 10 > 0) {
+			dimes = changeCents / 10;
+			changeCents = changeCents - (dimes * 10);
+			System.out.println(changeCents);
+			System.out.println("$0.10 x " + dimes);
+			
+			
+		}
+		if (changeCents - 5 > 0) {
+			nickels = changeCents / 5;
+			changeCents = changeCents - (nickels * 5);
+			System.out.println(changeCents);
+			System.out.println("$0.5 x " + nickels);
+			
+			
+		}
+		if (changeCents - 1 >= 0) {
+			pennies = changeCents;
+			changeCents = changeCents - (pennies);
+			System.out.println(changeCents);
+			System.out.println("$0.1 x " + pennies);
+			
+		}
+		System.out.println(changeCents);
+		
+}
+}
+}
 	
 
 
